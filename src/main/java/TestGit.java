9d436a32ad3a0bed79aec4ec697b83b09a6d8cf5
linjinyu <806166502@QQ.COM>
2022-08-07 12:22:27 +0800

    test3

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -1,5 +1,7 @@
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("haha");
+        System.out.println("test2");
+        System.out.println("3333");
     }
 }
