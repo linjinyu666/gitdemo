773d0c09372797b49648a904b487de20b95a7f0d
linjinyu <806166502@QQ.COM>
2022-08-07 13:47:55 +0800

    hot fix commit 2

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -1,7 +1,9 @@
-public class TestGit {
+public class  TestGit {
     public static void main(String[] args) {
         System.out.println("haha");
         System.out.println("test4");
         System.out.println("hot-fix test");
+
+        System.out.println("hotfix test1");
     }
 }
